867a234c44f307fc02e85a6747da06b3a0c8a9f1
Kristoferlatt <42769298+Kristoferlatt@users.noreply.github.com>
2023-02-09 17:11:21 +0200

    Add files via upload

---
+++ ConsoleApp5/ConsoleApp5/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stopwatch");
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            for (int i = 0; i < 1000; i++)
+            {
+                System.Threading.Thread.Sleep(10);
+                Console.WriteLine(i);
+
+
+            }
+            stopwatch.Stop();
+
+            Console.WriteLine("Time elapsed:{0:hh\\:mm\\:ss}", stopwatch.Elapsed);
+        }
+    }
+}
---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Näita maksimaalset numbrit");
+
+            int a = 700;
+            int b = 400;
+            int ret;
+
+            Program program = new Program();
+            ret = program.DisplayMax(a, b);
+            Console.WriteLine("Maksimaalne väärtus on: {0}", ret );
+            Console.ReadLine();
+        }
+
+        public int DisplayMax(int num1, int num2)
+        {
+            int reasult;
+            
+            if(num1 > num2)
+            {
+                reasult = num1;
+            }
+            else
+            {
+                reasult = num2;
+                
+            }
+            return reasult; 
+        }
+    }
+}
