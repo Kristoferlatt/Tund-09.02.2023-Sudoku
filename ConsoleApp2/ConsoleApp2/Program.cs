8bce138146283c23606194358cb29350a62f833f
Kristoferlatt <42769298+Kristoferlatt@users.noreply.github.com>
2023-02-09 15:51:11 +0200

    Add files via upload

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        float num, precent;
+        static void Main(string[] args)
+        {
+           Program program = new Program();
+            program.Accept();
+            program.Print();
+            Console.ReadLine(); 
+
+        }
+
+        public void Accept()
+        {
+            Console.WriteLine("Sisesta hinne: ");
+            num = float.Parse(Console.ReadLine());
+        }
+
+        public void Print()
+        {
+            precent = (float)num / 850 * 100;
+
+            if (precent < 35)
+            {
+                Console.WriteLine("Sorry! You failed. Your mark is "+ precent);
+            }
+            else if (precent >35 && precent < 50)
+            {
+                Console.WriteLine("You got grade C anf your % mark is " + precent);
+            }
+            else if (precent > 50 && precent < 60)
+            {
+                Console.WriteLine("You got grade B anf your % mark is " + precent);
+            }
+            else if (precent >60 && precent < 75)
+            {
+                Console.WriteLine("You got grade A anf your % mark is " + precent);
+            }
+        }
+    }
+}
---
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+       
+
+            int[,] matrix = new int[3, 3];
+            FillMatrix(matrix);
+        }
+
+        public static void FillMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                   var result = matrix[i, j] = -1;
+                    Console.WriteLine(result);
+                }
+
+            }
+        }
+    }
+}
